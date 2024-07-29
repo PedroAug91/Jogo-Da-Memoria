519ca0f55688baec7c788d15b64be25d33aa1423
vicosta28 <122642656+vicosta28@users.noreply.github.com>
2024-07-29 11:56:57 -0300

    att- VOU PULAR DE UMA PONTE

--- script.js
+++ script.js
@@ -8,4 +8,29 @@ function createBoard() {
     }
 }
 
+const creatCard = () =>{
+    const grid = document.createElement('div');
+    const frente = document.createElement('div');
+    const verso = document.createElement('div');
+
+    grid,className = 'grid';
+    frente.className = 'frente';
+    verso.className = 'verso';
+
+    card.appendChild(frente);
+    card.appendChild(verso);
+    grid.appendChild(card);
+
+
+}
+
+const imgs = [
+'ryan-1',
+'ryan-2',
+'ryan-3',
+'ryan-4',
+'ryan-5'
+];
+
 createBoard()
+creatCard()
