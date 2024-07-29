4034f4cccb248a17c619c35858645aef936e1330
Pedro Augusto <95659638+PedroAug91@users.noreply.github.com>
2024-07-29 16:08:52 -0300

    att
    Co-Authored-By: vicosta28 <122642656+vicosta28@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 var gameBoard = document.getElementById("game-board")
 
+const imgs = [
+    "url('./imgs/ryan-1.jpg')",
+    "url('./imgs/ryan-2.jpg')",
+    "url('./imgs/ryan-3.jpg')",
+    "url('./imgs/ryan-4.jpg')",
+    "url('./imgs/ryan-5.jpg')",
+    "url('./imgs/ryan-1.jpg')",
+    "url('./imgs/ryan-2.jpg')",
+    "url('./imgs/ryan-3.jpg')",
+    "url('./imgs/ryan-4.jpg')",
+    "url('./imgs/ryan-5.jpg')",
+];
+
+
+function flipCard(id, index) {
+    
+    let button = document.getElementById(id)
+    button.classList.remove("flipped")
+    button.style.backgroundImage = imgs[index - 1]
+}
+
 function createBoard() {
     for (let i = 1; i <= 10; i++) {
         let button = document.createElement("button")
         button.classList.add("card", "flipped")
+        button.id = i
+        button.type = "button"
+        button.addEventListener('click', function() {
+            flipCard(this.id, i); // Envie o ID do botão como parâmetro
+        })
         gameBoard.appendChild(button)
     }
 }
 
-const creatCard = () =>{
-    const grid = document.createElement('div');
-    const frente = document.createElement('div');
-    const verso = document.createElement('div');
-
-    grid,className = 'grid';
-    frente.className = 'frente';
-    verso.className = 'verso';
-
-    card.appendChild(frente);
-    card.appendChild(verso);
-    grid.appendChild(card);
-
-
+function resetCards() {
+    for(let i = 1; i <= 10; i++) {
+        let button = document.getElementById(i)
+        button.classList.add("flipped")
+        button.style.backgroundImage = ""
+    }
 }
 
-const imgs = [
-'ryan-1',
-'ryan-2',
-'ryan-3',
-'ryan-4',
-'ryan-5'
-];
 
 createBoard()
-creatCard()
